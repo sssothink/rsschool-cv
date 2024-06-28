7bef17031a1e1f066920259b1591a2cc40e839d5
Sothink <rebenkov2002@mail.ru>
2024-06-29 00:18:44 +0300

    fix: section contact info

---
+++ app.js
@@ -0,0 +1,6 @@
+document.querySelector("#discord").addEventListener("click", function () {
+	navigator.clipboard.writeText("452507467046191105");
+	alert(
+		"My user ID (452507467046191105) has been copied to search in Discord :)"
+	);
+});
